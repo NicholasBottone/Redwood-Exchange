e1722e621300ad39551a65c0e99e130f7bf2ac57
Nicholas Bottone <nick@bottone.io>
2021-08-08 23:16:09 -0400

    Fix some stuff in tests

--- test/dex_test.js
+++ test/dex_test.js
@@ -1,13 +1,121 @@
-const { expectRevert } = require('@openzeppelin/test-helpers');
-const Pin = artifacts.require('dummy/Pin.sol');
-const Zrx = artifacts.require('dummy/Zrx.sol');
-const Exc = artifacts.require('Exc.sol');
+const { expectRevert } = require("@openzeppelin/test-helpers");
+const Pin = artifacts.require("dummy/Pin.sol");
+const Zrx = artifacts.require("dummy/Zrx.sol");
+const Exc = artifacts.require("Exc.sol");
 
 const SIDE = {
     BUY: 0,
-    SELL: 1
+    SELL: 1,
 };
 
-contract('Exc', (accounts) => {
+contract("Exc", (accounts) => {
+    let pin, zrx, exc;
+    const [trader1, trader2] = [accounts[1], accounts[2]];
+    const [PIN, ZRX] = ["PIN", "ZRX"].map((ticker) => web3.utils.fromAscii(ticker));
 
+    beforeEach(async () => {
+        [pin, zrx] = await Promise.all([Pin.new(), Zrx.new()]);
+        exc = await Exc.new();
+    });
+
+    it("getOrders", async () => {
+        const balance = 1000;
+        await exc.addToken(PIN, pin.address);
+        await exc.addToken(ZRX, zrx.address);
+        await pin.mint(trader1, balance);
+        await pin.approve(exc.address, balance, { from: trader1 });
+        await exc.deposit(balance, PIN, { from: trader1 });
+        const amount = 10;
+        const price = 5;
+        await exc.makeLimitOrder(ZRX, amount, price, 0, { from: trader1 });
+        const order = await exc.getOrders(ZRX, 0);
+        assert.equal(order[0].price, price);
+        assert.equal(order[0].amount, amount);
+        assert.equal(order.length, 1);
+    });
+
+    it("addToken", async () => {
+        let tokens0 = await exc.getTokens();
+        assert.equal(tokens0.length, 0); // check empty list
+        await exc.addToken(PIN, pin.address);
+        let tokens1 = await exc.getTokens();
+        assert.equal(tokens1.length, 1);
+        await exc.addToken(ZRX, zrx.address);
+        let tokens2 = await exc.getTokens();
+        assert.equal(tokens2.length, 2);
+    });
+
+    it("deposit", async () => {
+        const balance = 1000;
+        await exc.addToken(PIN, pin.address);
+        await pin.mint(trader1, balance);
+        await pin.approve(exc.address, balance, { from: trader1 });
+        await exc.deposit(1000, PIN, { from: trader1 });
+        const trader1Balance = await exc.traderBalances.call(trader1, PIN);
+        assert.equal(parseInt(trader1Balance), balance);
+    });
+
+    it("withdraw", async () => {
+        const balance = 1000;
+        await exc.addToken(PIN, pin.address);
+        await pin.mint(trader1, balance);
+        await pin.approve(exc.address, balance, { from: trader1 });
+        await pin.deposit(500, PIN, { from: trader1 });
+        await pin.withdraw(500, PIN, { from: trader1 });
+        const trader1Balance = await exc.traderBalances.call(trader1, PIN);
+        assert.equal(parseInt(trader1Balance), 0);
+    });
+
+    it("makeLimitOrder", async () => {
+        const balance = 50;
+        await exc.addToken(PIN, pin.address);
+        await exc.addToken(ZRX, zrx.address);
+        await pin.mint(trader2, balance);
+        await pin.approve(exc.address, balance, { from: trader2 });
+        await exc.deposit(balance, PIN, { from: trader1 });
+        await exc.makeLimitOrder(ZRX, 10, 5, 0, { from: trader2 });
+        // const order = await exc.getOrders(ZRX, 0);
+        const amount = 10;
+        const price = 5;
+        await exc.makeLimitOrder(ZRX, amount, price, 0, { from: trader2 });
+        const order = await exc.getOrders(ZRX, 0);
+        assert.equal(order[0].price, price);
+        assert.equal(order[0].amount, amount);
+        assert.equal(order.length, 1);
+    });
+
+    it("deleteLimitOrder", async () => {
+        const balance = 50;
+        await exc.addToken(PIN, pin.address);
+        await exc.addToken(ZRX, zrx.address);
+        await pin.mint(trader1, balance);
+        await pin.approve(exc.address, balance, { from: trader1 });
+        await exc.deposit(balance, PIN, { from: trader1 });
+        await exc.makeLimitOrder(ZRX, 10, 5, 0, { from: trader1 });
+        const amount = 10;
+        const price = 5;
+        await exc.makeLimitOrder(ZRX, amount, price, 0, { from: trader1 });
+        await exc.deleteLimitOrder(0, ZRX, 0, { from: trader1 });
+        const orders = await exc.getOrders(ZRX, 0);
+        assert.equal(orders.length, 0);
+    });
+
+    it("makeMarketOrder", async () => {
+        const balance = 1000;
+        await exc.addToken(PIN, pin.address);
+        await exc.addToken(ZRX, zrx.address);
+        await pin.mint(trader1, balance);
+        await pin.approve(exc.address, balance, { from: trader1 });
+        await exc.deposit(balance, PIN, { from: trader1 });
+        await exc.makeLimitOrder(ZRX, 1, 50, 0, { from: trader1 });
+        await exc.makeLimitOrder(ZRX, 2, 25, 0, { from: trader1 });
+        await zrx.mint(trader2, balance);
+        await zrx.approve(exc.address, balance, { from: trader2 });
+        await exc.deposit(balance, ZRX, { from: trader2 });
+        await exc.makeMarketOrder(ZRX, 2, 10, { from: trader2 });
+        const trader1PINBalance = await exc.traderBalances.call(trader1, PIN);
+        assert.equal(trader1PINBalance, 100);
+        const trader2ZRXBalance = await exc.traderBalances.call(trader2, ZRX);
+        assert.equal(trader2ZRXBalance, 980);
+    });
 });
--- test/pool_test.js
+++ test/pool_test.js
@@ -1,30 +1,26 @@
-const Pin = artifacts.require('dummy/Pin.sol');
-const Zrx = artifacts.require('dummy/Zrx.sol');
-const Exc = artifacts.require('Exc.sol');
-const Fac = artifacts.require('Factory.sol')
-const Pool = artifacts.require('Pool.sol')
+const Pin = artifacts.require("dummy/Pin.sol");
+const Zrx = artifacts.require("dummy/Zrx.sol");
+const Exc = artifacts.require("Exc.sol");
+const Fac = artifacts.require("Factory.sol");
+const Pool = artifacts.require("Pool.sol");
 
-contract('Pool', (accounts) => {
+contract("Pool", (accounts) => {
     let pin, zrx, exc;
     const [trader1, trader2] = [accounts[1], accounts[2]];
-    const [PIN, ZRX] = ['PIN', 'ZRX']
-        .map(ticker => web3.utils.fromAscii(ticker));
+    const [PIN, ZRX] = ["PIN", "ZRX"].map((ticker) => web3.utils.fromAscii(ticker));
 
-    beforeEach(async() => {
-        ([pin, zrx] = await Promise.all([
-            Pin.new(),
-            Zrx.new()
-        ]));
+    beforeEach(async () => {
+        [pin, zrx] = await Promise.all([Pin.new(), Zrx.new()]);
         exc = await Exc.new();
-        fac = await Fac.new()
+        fac = await Fac.new();
     });
 
-    it("initialize + depsoit", async () => {
+    it("initialize + deposit", async () => {
         const pool = await Pool.at(log.args.pair);
         const balance = 1000;
         await pin.mint(trader1, balance);
-        await pin.approve(pool.address, balance, {from: trader1});
-        await pool.deposit(balance, balance, {from: trader1});
+        await pin.approve(pool.address, balance, { from: trader1 });
+        await pool.deposit(balance, balance, { from: trader1 });
         const poolBalancePIN = await exc.traderBalances.call(pool.address, PIN);
         const poolBalanceZRX = await exc.traderBalances.call(pool.address, ZRX);
         assert.equal(parseInt(poolBalancePIN), balance);
@@ -34,14 +30,14 @@ contract('Pool', (accounts) => {
     it("withdraw", async () => {
         const balance = 1000;
         await pin.mint(trader1, balance);
-        pin.approve(pool.address, balancem {from: trader1});
+        pin.approve(pool.address, balance, { from: trader1 });
         zrx.mint(trader1, balance);
-        zrx.approve(pool.address, balance, {from: trader1});
-        await pool.deposit(balance, balance, {from: trader1});
+        zrx.approve(pool.address, balance, { from: trader1 });
+        await pool.deposit(balance, balance, { from: trader1 });
         const orders1 = await exc.getOrders(ZRX, 0);
-        await pool.withdraw(balance, balance, {from: trader1});
+        await pool.withdraw(balance, balance, { from: trader1 });
         const orders2 = await exc.getOrders(ZRX, 0);
-        assert(order1[0].ticker == order2[0].ticker);
-        assert(order1[0].price != orders2[0].price);
+        assert.equal(orders1[0].ticker, orders2[0].ticker);
+        assert.ok(orders1[0].price != orders2[0].price);
     });
 });
