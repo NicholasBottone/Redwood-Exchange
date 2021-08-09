4fd3828ee2766e6f5b27b0973671a7d60a44b8ca
schornb <55115049+schornb@users.noreply.github.com>
2021-08-08 23:01:44 -0400

    pool tests

---
+++ test/pool_test.js
@@ -0,0 +1,47 @@
+const Pin = artifacts.require('dummy/Pin.sol');
+const Zrx = artifacts.require('dummy/Zrx.sol');
+const Exc = artifacts.require('Exc.sol');
+const Fac = artifacts.require('Factory.sol')
+const Pool = artifacts.require('Pool.sol')
+
+contract('Pool', (accounts) => {
+    let pin, zrx, exc;
+    const [trader1, trader2] = [accounts[1], accounts[2]];
+    const [PIN, ZRX] = ['PIN', 'ZRX']
+        .map(ticker => web3.utils.fromAscii(ticker));
+
+    beforeEach(async() => {
+        ([pin, zrx] = await Promise.all([
+            Pin.new(),
+            Zrx.new()
+        ]));
+        exc = await Exc.new();
+        fac = await Fac.new()
+    });
+
+    it("initialize + depsoit", async () => {
+        const pool = await Pool.at(log.args.pair);
+        const balance = 1000;
+        await pin.mint(trader1, balance);
+        await pin.approve(pool.address, balance, {from: trader1});
+        await pool.deposit(balance, balance, {from: trader1});
+        const poolBalancePIN = await exc.traderBalances.call(pool.address, PIN);
+        const poolBalanceZRX = await exc.traderBalances.call(pool.address, ZRX);
+        assert.equal(parseInt(poolBalancePIN), balance);
+        assert.equal(parseInt(poolBalanceZRX), balance);
+    });
+
+    it("withdraw", async () => {
+        const balance = 1000;
+        await pin.mint(trader1, balance);
+        pin.approve(pool.address, balancem {from: trader1});
+        zrx.mint(trader1, balance);
+        zrx.approve(pool.address, balance, {from: trader1});
+        await pool.deposit(balance, balance, {from: trader1});
+        const orders1 = await exc.getOrders(ZRX, 0);
+        await pool.withdraw(balance, balance, {from: trader1});
+        const orders2 = await exc.getOrders(ZRX, 0);
+        assert(order1[0].ticker == order2[0].ticker);
+        assert(order1[0].price != orders2[0].price);
+    });
+});
